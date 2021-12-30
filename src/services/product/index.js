72592e453836a7b94f278850fdab8e2aacbd7548
Roby Morgan <roby.c.morgan@gmail.com>
2021-12-30 16:00:44 +0100

    added get product by id route.

--- src/services/product/index.js
+++ src/services/product/index.js
@@ -1,7 +1,13 @@
 /** @format */
 
 import express from "express";
-import { Product, productCategory, User, Review, Category } from "../../db/models/index.js";
+import {
+  Product,
+  productCategory,
+  User,
+  Review,
+  Category,
+} from "../../db/models/index.js";
 // import { Op, Sequelize } from "sequelize";
 const router = express.Router();
 
@@ -11,24 +17,28 @@ router
     try {
       const data = await Product.findAll({
         include: [
-        {
-          model: Category, through: { attributes: [] },
-          where: { ...(req.query.category && {
-              name: { [Op.in]: req.category.name.split(",")}
-          })}
-        },
-        { model: Review, include: User},User,
+          {
+            model: Category,
+            through: { attributes: [] },
+            where: {
+              ...(req.query.category && {
+                name: { [Op.in]: req.category.name.split(",") },
+              }),
+            },
+          },
+          { model: Review, include: User },
+          User,
         ],
         where: {
           ...(req.query.search && {
             [Op.or]: [
-              {product_name: { [Op.ilike]: `%${req.query.search}%`},},
-              {content: { [Op.ilike]: `%${req.query.search}%`},},
-              {"$user.name$": { [Op.ilike]: "%" + req.query.search + "%"},}
-            ]
-          })
-        }
-    });
+              { product_name: { [Op.ilike]: `%${req.query.search}%` } },
+              { content: { [Op.ilike]: `%${req.query.search}%` } },
+              { "$user.name$": { [Op.ilike]: "%" + req.query.search + "%" } },
+            ],
+          }),
+        },
+      });
       res.send(data);
     } catch (e) {
       console.log(e);
@@ -38,7 +48,7 @@ router
   .post(async (req, res, next) => {
     try {
       const { categoryId, ...rest } = req.body;
-      console.log(rest)
+      console.log(rest);
       const product = await Product.create(req.body);
       if (product) {
         console.log(product.id);
@@ -61,13 +71,13 @@ router.route("/bulk").post(async (req, res, next) => {
   try {
     const { categoryId, ...rest } = req.body;
     const product = await Product.bulkCreate(req.body);
-    if(product) {
+    if (product) {
       const dataToInsert = categoryId.map((id) => ({
         categoryId: id,
         productId: product.id,
       }));
       const data = await productCategory.bulkCreate(dataToInsert);
-        res.send({ product, productCategory: data });
+      res.send({ product, productCategory: data });
     }
     res.send(data);
   } catch (error) {
@@ -80,6 +90,12 @@ router
   .route("/:id")
   .get(async (req, res, next) => {
     try {
+      if (req.params.id) {
+        const data = await Product.findOne({
+          where: { id: req.params.id },
+        });
+        res.send(data);
+      }
     } catch (e) {
       console.log(e);
       next(e);
